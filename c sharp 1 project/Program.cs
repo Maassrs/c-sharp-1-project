fc5530eb5d8617d5bfcde3ed40a8f239aa883829
Maassrs <m.essers@sintlucasedu.nl>
2024-09-06 15:36:02 +0200

    Initial commit

---
+++ c sharp 1 project/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace c_sharp_1_project;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //this is a comment. use me
+
+        // variables
+        int age = 15;
+
+        string name = "Maas";
+
+        //A way to write stuff to screen
+        Console.WriteLine("hello my name is " + name);
+        Console.WriteLine("My age is " + age);
+
+        Console.WriteLine("Press enter to exit.");
+        Console.ReadLine();
+    }
+}
+
