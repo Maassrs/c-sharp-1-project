7d68df1cbe2ad97ff9cde2e6e36d247e18f87fbd
Maassrs <m.essers@sintlucasedu.nl>
2024-09-12 12:59:59 +0200

    made some things

--- c sharp 1 project/Program.cs
+++ c sharp 1 project/Program.cs
@@ -10,10 +10,64 @@ class Program
         int age = 15;
 
         string name = "Maas";
+        string input;
+       
+
 
         //A way to write stuff to screen
         Console.WriteLine("hello my name is " + name);
         Console.WriteLine("My age is " + age);
+        Console.WriteLine("The wizard " + name + " is super strong and he wants to take over the world with his power.");
+        Console.WriteLine("1=win    2=lose");
+        Console.WriteLine();
+        input = Console.ReadLine(); //100000000
+
+
+        if (input == "1")
+        {
+            Console.WriteLine("he takes over the world and now is every one hes slave.");
+        }
+        else if (input == "2")
+        {
+            Console.WriteLine("he got beated by MR BIBS and he got smashed in to the ground and he died.");
+        }
+        else
+        {
+            Console.WriteLine("error put 1 or 2");
+        }
+
+
+
+
+
+
+        Console.WriteLine();
+            Console.WriteLine("the wizard " + name + " was a good man but the whole world exploded and there are some aliens who revive him and now is he getting revenge");
+        Console.WriteLine(" 1=takes epic revange    2=he gets a hard attack and dies");
+        Console.WriteLine();
+        input = Console.ReadLine();
+
+
+        if (input == "1") 
+        {
+            Console.WriteLine("he destroys universe.");
+        }
+        else if (input == "2")
+        {
+            Console.WriteLine("he gets hard attack.");
+        }
+        else
+        {
+            Console.WriteLine("error put 1 or 2");
+        }
+
+
+       
+
+
+
+
+
 
         Console.WriteLine("Press enter to exit.");
         Console.ReadLine();
